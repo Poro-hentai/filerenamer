f4aa5b3e3fc206392c2929922a4ea4f3be8a5d78
Poro-hentai <shadowsama128@gmail.com>
2025-06-29 20:05:14 +0530

    Update utils.py

--- bot/plugins/utils.py
+++ bot/plugins/utils.py
@@ -1,1 +1,14 @@
+import json
+import os
+
+def load_json(path):
+    if not os.path.exists(path):
+        with open(path, "w") as f:
+            json.dump({}, f)
+    with open(path, "r") as f:
+        return json.load(f)
+
+def save_json(path, data):
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
 
