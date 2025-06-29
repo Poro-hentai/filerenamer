14173e80ab0a23a4718f0246402b694314a29c4a
Poro-hentai <shadowsama128@gmail.com>
2025-06-29 20:02:15 +0530

    Update start.py

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -1,1 +1,16 @@
 
+from pyrogram import Client, filters
+from config import Config
+import json
+
+@Client.on_message(filters.command("start"))
+async def start_command(client, message):
+    await message.reply_text(Config.START_TEXT)
+
+@Client.on_message(filters.command("help"))
+async def help_command(client, message):
+    await message.reply_text(Config.HELP_TEXT)
+
+@Client.on_message(filters.command("about"))
+async def about_command(client, message):
+    await message.reply_text(Config.ABOUT_TEXT)
